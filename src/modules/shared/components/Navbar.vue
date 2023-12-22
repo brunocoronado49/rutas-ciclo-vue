29add0a8edf1f5fd582b1acbc29ece1e9094f7d2
Francisco Rangel <francorangelcoronado@gmail.com>
2023-12-21 23:24:55 -0600

    creando ruoter-link personalizados

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,10 +1,36 @@
 <template>
     <div>
-        <router-link :to="{ name: 'home' }">Pokemon List</router-link>
+        <!-- <router-link :to="{ name: 'home' }">Pokemon List</router-link>
         <router-link :to="{ name: 'pokemon-id', params: { id: 85 } }">Pokemon Id</router-link>
-        <router-link :to="{ name: 'about' }">About</router-link>
+        <router-link :to="{ name: 'about' }">About</router-link> -->
+        <CustomLink v-for="link in links" :key="link.to" :link="link" />
     </div>
 </template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            links: [
+                { to: '/home', name: 'Pokemons' },
+                { to: '/pokemonid/50', name: 'By ID' },
+                { to: '/about', name: 'About' },
+                { to: 'https://google.com', name: 'Google' },
+            ],
+        }
+    },
+    components: {
+        CustomLink: defineAsyncComponent(() => import(
+            /* webpackChunkName: CustomLink */
+            './CustomLink.vue'
+        ))
+    }
+}
+
+</script>
+
 
 <style scoped>
     div {
@@ -17,7 +43,7 @@
         margin: 0 15px;
     }
 
-    a.router-link-exact-active {
+    /* a.router-link-exact-active {
         color: #42b983;
-    }
+    } */
 </style>
